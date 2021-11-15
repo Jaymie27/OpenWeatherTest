dcb187eb9ebf1788a3127d0f7b0fe3efb0c8d531
Jaymie27 <j_deschenes27@hotmail.com>
2021-11-14 19:28:10 -0500

    WPF et ViewModel complété 1274000

--- WeatherApp/ViewModels/WindDataViewModel.cs
+++ WeatherApp/ViewModels/WindDataViewModel.cs
@@ -4,18 +4,25 @@ using WeatherApp.Services;
 
 namespace WeatherApp.ViewModels
 {
-    public class WindDataViewModel
+    public class WindDataViewModel : BaseViewModel
     {
+        private WindDataModel currentData;
         public IWindDataService WindDataService { get; set; }
         public DelegateCommand<string> GetDataCommand { get; set; }
-        public WindDataModel CurrentData { get; set; }
+        public WindDataModel CurrentData { get => currentData; set { currentData = value; OnPropertyChanged(); } }
 
         public double KPHtoMPS(double kph) => kph * 1.0 / 36.0;
         public double MPStoKPH(double mps) => mps * 3.6;
         
-        public WindDataModel()
+        public WindDataViewModel()
         {
-            
+            WindDataService = new OpenWeatherService(AppConfiguration.GetValue("ApiKey"));
+            GetDataCommand = new DelegateCommand<string>(GetData);
+        }
+
+        private async void GetData(string s)
+        {
+            CurrentData = await WindDataService.GetDataAsync();
         }
     }
 }
